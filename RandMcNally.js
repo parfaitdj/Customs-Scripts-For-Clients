9393db3c0f9e3c2081224e83e09e69e8cc8ece44
parfaitdj <107577498+parfaitdj@users.noreply.github.com>
2022-06-16 13:14:06 +0000

    Create RandMcNally.js

---
+++ RandMcNally.js
@@ -0,0 +1,10 @@
+<script>
+  hbspt.forms.create({
+	portalId: "20758866",
+    formId: "hsForm_51b2accf-8b11-4b7a-b9c7-44875481f74e",
+    onFormSubmit: function($form) {
+    //Ask MCFX to Capture the form on Submission
+        mcfx('capture', $form.get(0));
+        } 
+});
+</script>
